d94da753f92ef0b94679aa07b9e45d61eebe30ff
hua <Guanhua_Li@foxmail.com>
2017-11-04 06:54:25 +0800

    frontend mostly

---
+++ js/index.js
@@ -0,0 +1,204 @@
+// External Files:
+// https://api.html5media.info/1.1.8/html5media.min.js (enables <video> and <audio> tags in all major browsers)
+// https://cdn.plyr.io/2.0.13/plyr.js
+
+
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+jQuery(function ($) {
+    'use strict'
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+       var data = [{
+                "track": 1,
+                "name": "All This Is - Joe L.'s Studio",
+                "length": "2:46",
+                "file": "JLS_ATI"
+            }, {
+                "track": 2,
+                "name": "The Forsaken - Broadwing Studio (Final Mix)",
+                "length": "8:31",
+                "file": "BS_TF"
+            }, {
+                "track": 3,
+                "name": "All The King's Men - Broadwing Studio (Final Mix)",
+                "length": "5:02",
+                "file": "BS_ATKM"
+            }, {
+                "track": 4,
+                "name": "The Forsaken - Broadwing Studio (First Mix)",
+                "length": "8:32",
+                "file": "BSFM_TF"
+            }, {
+                "track": 5,
+                "name": "All The King's Men - Broadwing Studio (First Mix)",
+                "length": "5:05",
+                "file": "BSFM_ATKM"
+            }, {
+                "track": 6,
+                "name": "All This Is - Alternate Cuts",
+                "length": "2:49",
+                "file": "AC_ATI"
+            }, {
+                "track": 7,
+                "name": "All The King's Men (Take 1) - Alternate Cuts",
+                "length": "5:45",
+                "file": "AC_ATKMTake_1"
+            }, {
+                "track": 8,
+                "name": "All The King's Men (Take 2) - Alternate Cuts",
+                "length": "5:27",
+                "file": "AC_ATKMTake_2"
+            }, {
+                "track": 9,
+                "name": "Magus - Alternate Cuts",
+                "length": "5:46",
+                "file": "AC_M"
+            }, {
+                "track": 10,
+                "name": "The State Of Wearing Address (fucked up) - Alternate Cuts",
+                "length": "5:25",
+                "file": "AC_TSOWAfucked_up"
+            }, {
+                "track": 11,
+                "name": "Magus - Popeye's (New Years '04 - '05)",
+                "length": "5:54",
+                "file": "PNY04-05_M"
+            }, {
+                "track": 12,
+                "name": "On The Waterfront - Popeye's (New Years '04 - '05)",
+                "length": "4:41",
+                "file": "PNY04-05_OTW"
+            }, {
+                "track": 13,
+                "name": "Trance - Popeye's (New Years '04 - '05)",
+                "length": "13:17",
+                "file": "PNY04-05_T"
+            }, {
+                "track": 14,
+                "name": "The Forsaken - Popeye's (New Years '04 - '05)",
+                "length": "8:13",
+                "file": "PNY04-05_TF"
+            }];
+        var zz = function(data,flag){
+        $('#plList').html("");
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://archive.org/download/mythium/',
+            extension = '',
+            tracks = data,
+
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackLength = value.length;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                } else {
+                    trackNumber = '' + trackNumber;
+                }
+                $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').bind('play', function () {
+                playing = true;
+                npAction.text('Now Playing...');
+            }).bind('pause', function () {
+                playing = false;
+                npAction.text('Paused...');
+            }).bind('ended', function () {
+                npAction.text('Paused...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').click(function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').click(function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').click(function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+
+      };
+
+
+      //样例
+
+       // zz(data);
+       var racks=[{
+                "track": 1,
+                "name": "The Forsaken - Broadwing Studio (Final Mix)",
+                "length": "8:31",
+                "file": "BS_TF"
+          },{
+                "track": 2,
+                "name": "The Forsaken - Broadwing Studio (Final Mix)",
+                "length": "8:31",
+                "file": "BS_TF"
+          }];
+        zz(racks,true);
+
+
+
+        var testTime = function(){
+            var htmlobj=$.ajax({url:"https://www.juhe.cn/loginStatus",dataType:'jsonp',async:false});
+            console.log(htmlobj.responseText)
+            // zz(data,false)
+
+
+        };
+        setInterval(testTime,5000);
+
+    }
+});
+
+//initialize plyr
+plyr.setup($('#audio1'), {});
